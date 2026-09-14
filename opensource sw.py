66fa336b907594da49946fcd8b1ba882f47448a8
GitHub shineunho1106 <puzzleboy1106@gmail.com>
2026-09-14 17:38:45 +0900

    this update is a test in open source sw class

--- opensource sw.py
+++ opensource sw.py
@@ -1,7 +1,19 @@
 def print_hello():
     print("hello world")
 
+print_hello()
 
-if __name__ == '__main__' :
-    print_hello()
+import turtle
 
+screen = turtle.Screen()
+screen.bgcolor("black")
+
+star = turtle.Turtle()
+star.color("yellow")
+star.speed(5)
+
+for i in range(5):
+    star.forward(200)
+    star.right(144)
+
+screen.exitonclick()
