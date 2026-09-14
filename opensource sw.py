c58734d369cd265bfd59a79bd8ad2bc6ee48931f
GitHub shineunho1106 <puzzleboy1106@gmail.com>
2026-09-14 17:45:56 +0900

    4th class test 2

--- opensource sw.py
+++ opensource sw.py
@@ -1,19 +1,10 @@
-def print_hello():
-    print("hello world")
+def add(x, y):
+    a = x + y
+    return a
 
-print_hello()
+def print_KIT():
+    print("KIT")
 
-import turtle
-
-screen = turtle.Screen()
-screen.bgcolor("black")
-
-star = turtle.Turtle()
-star.color("yellow")
-star.speed(5)
-
-for i in range(5):
-    star.forward(200)
-    star.right(144)
-
-screen.exitonclick()
+if __name__ == '__main__':
+    print_KIT()
+    print(add(1, 2))
